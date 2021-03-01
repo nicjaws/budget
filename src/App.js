0487d036ea36f926b723fb271c83e00e2f4b8f04
Nicolas Corbalan <outreef@hotmail.com>
2021-03-01 13:01:28 +0100

    adding Form and buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import {Container, Grid, Header, Segment, Statistic} from 'semantic-ui-react';
+import {Button, Container, Form, Grid, Header, Icon, Segment, Statistic} from 'semantic-ui-react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   return (
     <Container>
+
       <Header as='h1'>
         Budget
       </Header>
+
       <Statistic size="small">
         <Statistic.Label>
           Your Balance: 
@@ -16,6 +18,7 @@ function App() {
           2,550.53
         </Statistic.Value>
       </Statistic>
+
       <Segment textAlign="center">
         <Grid columns={2} divided>
           <Grid.Row>
@@ -42,6 +45,90 @@ function App() {
           </Grid.Row>
         </Grid>
       </Segment>
+
+      <Header as="h3">
+        History
+      </Header>
+      <Segment color="red">
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">
+              Something
+            </Grid.Column>
+            <Grid.Column width={3} textAlign="right">
+              $10,00
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name="edit" bordered />
+              <Icon name="trash"  bordered />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Segment color="green">
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">
+              Something else
+            </Grid.Column>
+            <Grid.Column width={3} textAlign="right">
+              $100,00
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name="edit" bordered />
+              <Icon name="trash"  bordered />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Segment color="red">
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign="left">
+              Something
+            </Grid.Column>
+            <Grid.Column width={3} textAlign="right">
+              $20,00
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name="edit" bordered />
+              <Icon name="trash"  bordered />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Header as="h3">
+        Add new transaction
+      </Header>
+      <Form unstackable>
+        <Form.Group>
+          <Form.Input 
+            icon="tags"
+            width={12}
+            label="Description"
+            placeholder="New shinny thing" 
+          />
+
+          <Form.Input
+            width={4}
+            label="Value"
+            placeholder="100.00"
+            icon="dollar"
+            iconPosition="left"
+            >
+          </Form.Input>
+        </Form.Group>
+        <Button.Group style={{marginTop: 20}}>
+          <Button>
+            Cancel
+          </Button>
+          <Button.Or/>
+          <Button primary>OK</Button>
+        </Button.Group>
+      </Form>
     </Container>
   );
 }
