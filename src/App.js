c11f0145f4ede2c5b85403aab184d4d4426e8d8c
Nicolas Corbalan <outreef@hotmail.com>
2021-03-02 00:07:51 +0100

    finish components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import {
-  Container, 
-  Grid, 
-  Icon, 
-  Segment, 
-  Statistic} from 'semantic-ui-react';
+  Container
+} from 'semantic-ui-react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
 import DisplayBalance from './components/DisplayBalance';
+import DisplayBalances from './components/DisplayBalances';
+import EntryLine from './components/EntryLine';
 
 function App() {
   return (
@@ -16,89 +15,18 @@ function App() {
 
      <MainHeader title="Budget"/>
 
-      <Statistic size="small">
-        <Statistic.Label>
-          Your Balance: 
-        </Statistic.Label>
-        <Statistic.Value>
-          2,550.53
-        </Statistic.Value>
-      </Statistic>
+      <DisplayBalance title="Your balance" value="2550.53" size="small" />
 
-      <Segment textAlign="center">
-        <Grid columns={2} divided>
-          <Grid.Row>
-           <Grid.Column>
-              
-              <DisplayBalance size="tiny" color="green" label="Incoming" value="1,045.50"/>
-
-           </Grid.Column> 
-           <Grid.Column>
-            <Statistic size="tiny" color="red">
-                <Statistic.Label style={{textAlign:"left"}}>
-                  Expenses:
-                </Statistic.Label>
-                <Statistic.Value>
-                  623.50
-                </Statistic.Value>
-              </Statistic>
-           </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+      <DisplayBalances />
 
       <MainHeader title="History" type="h3" />
 
-      <Segment color="red">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign="left">
-              Something
-            </Grid.Column>
-            <Grid.Column width={3} textAlign="right">
-              $10,00
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash"  bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+      <EntryLine description="Expense" value="$10,00"
+      isExpense />
 
-      <Segment color="green">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign="left">
-              Something else
-            </Grid.Column>
-            <Grid.Column width={3} textAlign="right">
-              $100,00
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash"  bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+      <EntryLine description="Income" value="$100,00"
+       />
 
-      <Segment color="red">
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign="left">
-              Something
-            </Grid.Column>
-            <Grid.Column width={3} textAlign="right">
-              $20,00
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name="edit" bordered />
-              <Icon name="trash"  bordered />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
 
       <MainHeader title="Add new transaction" type="h3" />
 
--- src/components/DisplayBalance.js
+++ src/components/DisplayBalance.js
@@ -2,11 +2,11 @@ import React from 'react'
 import {Statistic} from 'semantic-ui-react';
 
 
-function DisplayBalance({size, color, label, value }) {
+function DisplayBalance({ title, value, color="black", size="tiny" }) {
   return (
     <Statistic size={size} color={color}>
                 <Statistic.Label style={{textAlign:"left"}}>
-                  {label}
+                  {title}
                 </Statistic.Label>
                 <Statistic.Value>
                   {value}
