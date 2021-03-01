7f6280166240a7de22e759e020c5c6b3163240b0
Nicolas Corbalan <outreef@hotmail.com>
2021-03-02 00:55:25 +0100

    mapping entries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import {
-  Container
-} from 'semantic-ui-react';
+import { useState } from 'react';
+import {Container} from 'semantic-ui-react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import MainHeader from './components/MainHeader';
@@ -8,8 +7,14 @@ import NewEntryForm from './components/NewEntryForm';
 import DisplayBalance from './components/DisplayBalance';
 import DisplayBalances from './components/DisplayBalances';
 import EntryLine from './components/EntryLine';
+import EntryLines from './components/EntryLines';
+
+
+
+
 
 function App() {
+  const [entries, setsEntries] = useState(initialEntries);
   return (
     <Container>
 
@@ -21,12 +26,7 @@ function App() {
 
       <MainHeader title="History" type="h3" />
 
-      <EntryLine description="Expense" value="$10,00"
-      isExpense />
-
-      <EntryLine description="Income" value="$100,00"
-       />
-
+      <EntryLines entries={entries}/>
 
       <MainHeader title="Add new transaction" type="h3" />
 
@@ -37,3 +37,22 @@ function App() {
 }
 
 export default App;
+
+
+var initialEntries = [
+  {
+    description: "Work income",
+    value: "$100,00",
+    isExpense: false 
+  },
+  {
+    description: "Water Bill",
+    value: "$20,00",
+    isExpense: true
+  },
+  {
+    description: "Rent",
+    value: "$300,00",
+    isExpense: true
+  }
+]
