399002ff69d2f4049496b5f5279985cf29798034
Nicolas Corbalan <outreef@hotmail.com>
2021-03-01 02:02:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import {Container, Header, Statistic} from 'semantic-ui-react';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header as='h1'>
+        Budget
+      </Header>
+      <Statistic size="small">
+        <Statistic.Label>
+          Your Balance: 
+        </Statistic.Label>
+        <Statistic.Value>
+          2,550.53
+        </Statistic.Value>
+      </Statistic>
+    </Container>
   );
 }
 
